9ce522066d20e7167b1d6ebaf0b5fb89ae453b74
Nicholas Dover <dovern42@gmail.com>
2019-02-20 19:37:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,7 @@ class Shoe
   BRANDS = []
   def initialize(brand)
     @brand = brand
-    BRANDS.include?(brand) ? | BRANDS << brand
+    if BRANDS.include?(brand)! then BRANDS << brand
   end
 
   def cobble
